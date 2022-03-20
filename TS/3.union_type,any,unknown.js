50847e2b4ece4da2c18b5e280039855974cc73ce
성해호 <heyho9292@gmail.com>
2022-03-20 09:09:22 +0900

    function homework

--- TS/3.union_type,any,unknown.js
+++ TS/3.union_type,any,unknown.js
@@ -44,9 +44,9 @@ var 철수 = [user, age, married];
 // 학교.score[4] = false;
 // 학교.friend = ['Lee' , 학교.teacher]
 var 학교 = {
-  score: [100, 97, 84],
-  teacher: "Phil",
-  friend: "John",
+    score: [100, 97, 84],
+    teacher: "Phil",
+    friend: "John",
 };
 학교.score[4] = false;
 학교.friend = ["Lee", 학교.teacher];
